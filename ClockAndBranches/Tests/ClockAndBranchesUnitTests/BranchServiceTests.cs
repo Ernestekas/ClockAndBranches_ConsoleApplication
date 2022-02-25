90b2b7689b58d833b653d3250b72735443b62f45
Ernestekas <ernestekas@gmail.com>
2022-02-25 18:28:50 +0200

    Test for GetDeepestDreamLevel method.
    - Method doesn't pass test.

--- ClockAndBranches/Tests/ClockAndBranchesUnitTests/BranchServiceTests.cs
+++ ClockAndBranches/Tests/ClockAndBranchesUnitTests/BranchServiceTests.cs
@@ -1,14 +1,47 @@
+using ClockAndBranches.Models;
+using ClockAndBranches.Services;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ClockAndBranchesUnitTests
 {
     public class BranchServiceTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly BranchService _branchService = new BranchService();
 
+        [Fact]
+        public void GetDeepestDreamLevel_GivenObjectTree_ReturnsLevelOf4()
+        {
+            Dream main = new Dream()
+            {
+                Dreams = new List<Dream>()
+                {
+                    new Dream()
+                    {
+                        Dreams = new List<Dream>()
+                        {
+                            new Dream()
+                            {
+                                Dreams = new List<Dream>()
+                                {
+                                    new Dream()
+                                }
+                            }
+                        }
+                    },
+                    new Dream()
+                    {
+                        Dreams = new List<Dream>()
+                        {
+                            new Dream()
+                        }
+                    }
+                }
+            };
+
+            int level = _branchService.GetDeepestDreamLevel(main, 1);
+            Assert.Equal(4, level);
         }
     }
 }
